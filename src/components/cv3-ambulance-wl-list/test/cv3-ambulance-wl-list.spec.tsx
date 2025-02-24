98fa339a3a7fd36b5d31bd91bc1784239c8642de
Richard Kello <richardkello@eduroam-162-240.fiit.stuba.sk>
2025-02-24 10:43:23 +0100

    Changed declaration of wlList

--- src/components/cv3-ambulance-wl-list/test/cv3-ambulance-wl-list.spec.tsx
+++ src/components/cv3-ambulance-wl-list/test/cv3-ambulance-wl-list.spec.tsx
@@ -7,7 +7,7 @@ describe('cv3-ambulance-wl-list', () => {
       components: [Cv3AmbulanceWlList],
       html: `<cv3-ambulance-wl-list></cv3-ambulance-wl-list>`,
     });
-    const wlList = page.rootInstance as PfxAmbulanceWlList;
+    const wlList = page.rootInstance as Cv3AmbulanceWlList;
     const expectedPatients = wlList?.waitingPatients?.length
 
     const items = page.root.shadowRoot.querySelectorAll("md-list-item");
